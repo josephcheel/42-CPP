10cc58abd313a6d79aae587016ca509e70fb4a11
Joseph Cheel Nakashima <josephcheel@MacBook-Air-de-Joseph.local>
2023-06-01 15:25:54 +0200

    Finished

--- CPP_Module_00/ex00/megaphone.cpp
+++ CPP_Module_00/ex00/megaphone.cpp
@@ -13,7 +13,7 @@ int main(int ac, char **av)
 		for (int c = 0; c < int(str.length()); c++)
 		{
 			if (std::islower(av[arg][c]))
-				std::cout <<  std::toupper(av[arg][c]);
+				std::cout <<  char(std::toupper(av[arg][c]));
 			else
 				std::cout << av[arg][c];
 		}
