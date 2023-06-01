47d355ec3f38198f5ba01a40c5f856113cae52a5
Joseph Cheel Nakashima <josephcheel@MacBook-Air-de-Joseph.local>
2023-06-01 14:46:35 +0200

    Module 00

---
+++ CPP_Module_00/ex01/Contact.class.cpp
@@ -0,0 +1,9 @@
+#include <string>
+
+class Contact {
+	string first_name;
+	string last_name, nickname, phone number, and darkest secret. A saved contact can’t have empty fields.
+
+
+
+}
---
+++ CPP_Module_00/ex01/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+#include "PhoneBook.class.hpp"
+#include "Contact.class.hpp"
+
+int	main(void)
+{
+	std::cout << "My Awesome PhoneBook is started" << std::endl;
+
+}
