64d3ff6269d86bf75e12ab67c3255bc947a3ebff
Joseph42 <jcheel-n@student.42barcelona.com>
2024-01-10 19:45:09 +0100

    Better Main

--- CPP_Module_03/ex00/src/main.cpp
+++ CPP_Module_03/ex00/src/main.cpp
@@ -2,23 +2,34 @@
 
 int main(void)
 {
+	// Default Constructor
+	ClapTrap none;
+
+	
+	// String Constructors
 	ClapTrap Ally("Ally");
 	ClapTrap Enemy("Enemy");
 
+	// Copy constructor
+	ClapTrap Copy(Ally);
+
+	// Copy Assignment
+	none = Enemy;
+
 	// ClapTrap Ally attack ClapTrap Enemy
 	Ally.attack("Enemy");
-	// ClapTrap Enemy gets damage from ClapTrap Enemy
+	
+	// ClapTrap Enemy gets damage from ClapTrap Enemy and Repairs
 	Enemy.takeDamage(Ally.getAttackDamage());
-	// Enemy Repairs
 	Enemy.beRepaired(20);
 
 	// ClapTrap Enemy attack ClapTrap Ally
 	Enemy.attack("Joseph");
-	// ClapTrap Ally gets damage from ClapTrap Enemy
+	
+	// ClapTrap Ally gets damage from ClapTrap Enemy. Ally Can´t Repair
 	Ally.takeDamage(10);
-	// Ally Can´t Repair
 	Ally.beRepaired(30);
-	// Ally Can´t Repair
+	
+	// Ally Can´t Attack
 	Ally.attack("Enemy");
-
 }
