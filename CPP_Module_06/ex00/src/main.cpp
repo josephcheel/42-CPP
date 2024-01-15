83f0fb50c55eb2a493885ba3de8fb98ab3284c4d
Joseph42 <jcheel-n@student.42barcelona.com>
2024-01-15 19:48:00 +0100

    First test CPP06

---
+++ CPP_Module_06/ex00/src/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+bool	isnum(std::string str)
+{
+	size_t lenght = str.length();
+}
+
+int	main(int ac, char **av)
+{
+	std::string nbr;
+
+	if (ac <= 1)
+		return 0;
+	nbr = av[1];
+	if (nbr[nbr.length() - 1] == 'f')
+	{
+	
+	}
+
+		
+}
