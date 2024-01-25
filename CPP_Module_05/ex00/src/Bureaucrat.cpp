a1cda3d8f98e9d36ae9dfa7629b7511c5a4d06b9
josephcheel <josephcheelnakashima@gmail.com>
2024-01-25 03:01:02 +0100

    Change message Exception

--- CPP_Module_05/ex00/src/Bureaucrat.cpp
+++ CPP_Module_05/ex00/src/Bureaucrat.cpp
@@ -73,10 +73,10 @@ std::ostream &operator<<(std::ostream& stream, const Bureaucrat &copy)
 
 const char * Bureaucrat::GradeTooHighException::what() const throw()
 {
-	return ("GradeTooHighException: Grade Too High");
+	return ("Bureaucrat: Grade Too High");
 }
 
 const char * Bureaucrat::GradeTooLowException::what() const throw()
 {
-	return ("GradeTooLowException: Grade Too Low");
+	return ("Bureaucrat: Grade Too Low");
 }
