8afeea4d7e270f4f8d0cc8b6c0b782e9f954d5ca
josephcheel <josephcheelnakashima@gmail.com>
2024-01-25 02:26:59 +0100

    main Ex00

--- CPP_Module_05/ex00/src/main.cpp
+++ CPP_Module_05/ex00/src/main.cpp
@@ -2,15 +2,64 @@
 
 int main()
 {
+	std::cout << "------------CLASS BASICS---------------" << std::endl << std::endl;
+	{
+		//Default constructor
+		Bureaucrat instance1;
+
+		// Parameter constructor
+		Bureaucrat instance2("me", 150);
+
+		// Copy constructor
+		Bureaucrat instance3(instance2);
+
+		// Copy Assignment operator
+		instance1 = instance2;
+	
+		// Overload of the insertion («) operator
+		std::cout << instance1 << std::endl;
+		std::cout << instance2 << std::endl;
+		std::cout << instance1 << std::endl;
+	}
+	std::cout << std::endl << "-----------CLASS GETTERS--------------" << std::endl << std::endl;
+	{
+		Bureaucrat instance1("burrrrreeeacraat", 150);
+		std::cout << "Name: " << instance1.getName() << std::endl;
+		std::cout << "Grade: " << instance1.getGrade() << std::endl;
+	}
+	std::cout << std::endl << "-----------CLASS GETTERS--------------" << std::endl << std::endl;
+	{
+		Bureaucrat instance1("burrrrreeeacraat", 150);
+		std::cout << "Name: " << instance1.getName() << std::endl;
+		std::cout << "Grade: " << instance1.getGrade() << std::endl;
+	}
+	std::cout << std::endl << "-----------CLASS INCREMENT AND DECREMENT--------------" << std::endl << std::endl;
+	{
+		Bureaucrat instance1("burrrrreeeacraat", 150);
+		std::cout << "Name: " << instance1.getName() << std::endl;
+		std::cout << "Grade: " << instance1.getGrade() << std::endl;
+		instance1.incrementGrade();
+		instance1.incrementGrade();
+		instance1.incrementGrade();
+		std::cout << "Grade: " << instance1.getGrade() << std::endl;
+		instance1.decrementGrade();
+		std::cout << "Grade: " << instance1.getGrade() << std::endl;
+	}
+	std::cout << std::endl << "-----------CLASS EXCEPTION--------------" << std::endl << std::endl;
 	try
 	{
-		//Bureaucrat instance1("me", 151);
-		//Bureaucrat instance2("me", 0);
-		Bureaucrat instance3("me", 150);
-		std::cout << instance3 << std::endl;
-		instance3.decrementGrade();
-		std::cout << instance3 << std::endl;
+		// Constructor Exception Too Low
+		Bureaucrat instance4("me", 152);
+		// Constructor Exception Too High
+		Bureaucrat instance5("me", 0);
 
+		// Increment Exception Too High
+		Bureaucrat instance6("me", 1);
+		instance6.incrementGrade();
+
+		// Decrement Exception Too Low
+		Bureaucrat instance7("me", 150);
+		instance7.decrementGrade();
 	}
 	catch(Bureaucrat::GradeTooLowException& e)
 	{
