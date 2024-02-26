48df377397c40532543357d8f8b2db1795929748
Joseph42 <jcheel-n@student.42barcelona.com>
2024-02-26 17:18:08 +0100

    ex01 corrected

--- CPP_Module_07/ex00/src/main.cpp
+++ CPP_Module_07/ex00/src/main.cpp
@@ -27,6 +27,7 @@ int	main( void )
 	std::cout << "c = " << c << ", d = " << d << std::endl;
 	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
 	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl ;
+	::swap(c, d);
 	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
 	return 0;
 }
--- CPP_Module_07/ex01/inc/Iter.hpp
+++ CPP_Module_07/ex01/inc/Iter.hpp
@@ -1,11 +1,15 @@
 #pragma once
+# include <iostream>
 
-template<typename T, int N, void f(T)>
-void	iter(T Array[N])
+template<typename T>
+void	iter(T Array[], unsigned int len, void (*f)(T&))
 {
-	for(int i = 0; i < N; i++)
-	{
+	for(unsigned int i = 0; i < len; i++)
 		f(Array[i]);
-	}
 }
 
+template <typename T>
+void print(T a)
+{
+	std::cout << a << std::endl;
+}
