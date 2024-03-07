283e9fb85af6e89d84802828d39dd4fe6d7484dd
josephcheel <josephcheelnakashima@gmail.com>
2024-03-07 14:21:26 +0100

    change usage ex01

--- CPP_Module_09/ex01/src/main.cpp
+++ CPP_Module_09/ex01/src/main.cpp
@@ -5,7 +5,7 @@ int	main(int ac, char **av)
 	if (ac != 2)
 	{
 		std::cerr << "usage: ./RPN \"number number arithmetic-operator...\"" << std::endl;
-		std::cerr << "usage: example: ./RPN 7 7 * 2" << std::endl;
+		std::cerr << "usage: example: ./RPN \"7 7 * 7 -\"" << std::endl;
 		return 0;
 	}
 	RPN instance(static_cast<std::string>(av[1]));
